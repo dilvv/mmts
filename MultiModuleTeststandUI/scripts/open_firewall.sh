a2134f7259ec7b51de5adcf5ee3a8e72a48d1def
HONOR <honor@local.dev>
2026-07-22 14:01:34 +0800

    Convert MultiModuleTeststandUI to dev submodule

--- MultiModuleTeststandUI/scripts/open_firewall.sh
+++
@@ -1,5 +0,0 @@
-sudo systemctl status firewalld ## check service is opened or not. use start and enable to make it run permanently
-#sudo firewall-cmd --zone=public --add-port=5001/tcp ### temporally open
-sudo firewall-cmd --zone=public --add-port=5001/tcp --permanent ### changed permanant
-sudo firewall-cmd --reload
-sudo firewall-cmd --list-port ### it should show 5001/tcp
